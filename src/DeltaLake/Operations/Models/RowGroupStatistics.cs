0a57f72f4808f0ef660b532f0e09e2ab751f7ebf
Naveen Sharma <naveen.sharma@energyexemplar.com>
2025-04-04 12:29:35 +0530

    Refactor RowGroupStatistics to use record type
    Changed `RowGroupStatistics` from a static partial class to a regular record class, enabling instance creation. Updated the constructor and property definitions to align with the new record syntax while maintaining existing functionality.

--- src/DeltaLake/Operations/Models/RowGroupStatistics.cs
+++ src/DeltaLake/Operations/Models/RowGroupStatistics.cs
@@ -2,17 +2,15 @@
 using Parquet.Schema;
 
 namespace DeltaLake.Operations.Utils {
-    internal static partial class ParquetUtil {
-        public record RowGroupStatistics {
+    internal record RowGroupStatistics {
 
-            public RowGroupStatistics(long rowCount, Dictionary<DataField, DataColumnStatistics> columnStatistics) {
-                RowCount = rowCount;
-                ColumnStatistics = columnStatistics;
-            }
-
-            public long RowCount { get; init; }
-            public Dictionary<DataField, DataColumnStatistics> ColumnStatistics { get; init; }
-
+        public RowGroupStatistics(long rowCount, Dictionary<DataField, DataColumnStatistics> columnStatistics) {
+            RowCount = rowCount;
+            ColumnStatistics = columnStatistics;
         }
+
+        public long RowCount { get; init; }
+        public Dictionary<DataField, DataColumnStatistics> ColumnStatistics { get; init; }
+
     }
 }
